c3eba09940fd9efed84fe751ac81fc0428a762bd
Vishal Jakhmola <43874682+VishalJakhmola@users.noreply.github.com>
2018-10-05 05:47:00 -0700

    Update server1.py

--- server1.py
+++ server1.py
@@ -2,10 +2,13 @@ import socket
 import sys
 import ipgetter
 
+hostname=socket.gethostname()
+host=socket.gethostbyname(hostname)
 host=ipgetter.myip()
 port=int(sys.argv[1])
+wan=ipgetter.myip()
 
-print ("Your ip address is: " + host)
+print ("Your ip address is: " + wan)
 print ("Connecting on port: "+ str(port))
 
 
