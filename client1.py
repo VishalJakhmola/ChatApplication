f92766042a5f1a5afae8d79e9c6d757e0ad042de
Vishal Jakhmola <43874682+VishalJakhmola@users.noreply.github.com>
2018-10-05 06:04:36 -0700

    Update client1.py

--- client1.py
+++ client1.py
@@ -1,25 +1,21 @@
 import socket
 import sys
-def main():
+
     
-    ip=sys.argv[1]
-    port=sys.argv[2]
+ip=str(sys.argv[1])
+port=int(sys.argv[2])
 
-    s=socket.socket()
-    s.connect((ip,port))
+s=socket.socket()
+s.connect((ip,port))
 
-    message=input("-->").encode()
+message=input("-->").encode()
 
-    while(message!="bye"):
-        s.send(message)
-        data=s.recv(1024)
-        print("Message from server:"+data.decode())
+while(message!="bye"):
+    s.send(message)
+    data=s.recv(1024)
+    print("Message from server:"+data.decode())
 
-    s.close()
-
-if__name__=='__main__':
-    main()
-
+s.close()
 
 
 
